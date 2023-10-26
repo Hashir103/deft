df8e1fbc0575a5fc0cba93932ad4a60419f3cf0e
Hashir <50779880+Hashir103@users.noreply.github.com>
2023-10-26 13:31:40 -0400

    Revert "Update bot.py"
    This reverts commit b4d3b42c449c2b467b3ee2fe0e3fb6dfc98d5afa.

--- bot.py
+++ bot.py
@@ -44,8 +44,7 @@ async def get_data():
 @tasks.loop(hours=1)
 async def check_alive():
     channel = bot.get_channel(1166414794639822918)
-    message = f"I'm alive. Kelly is a clown :clown:"
-    await channel.send(message)
+    await channel.send("I'm alive. Kelly is still a clown :clown:")
 
 @check_alive.before_loop
 async def before_check_alive():
